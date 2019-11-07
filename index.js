5a8cd656a6ad0ac5bf8d3f05cf10752b4fc02603
zusinShinpei <kmh011225@naver.com>
2019-11-07 23:44:46 +0900

    cleanup eslintrc

--- index.js
+++ index.js
@@ -1,80 +1,107 @@
 module.exports = {
-  "extends": [
-    "eslint-config-react-native-wcandillon"
+  'extends': [
+    'eslint-config-react-native-wcandillon',
   ],
-  "parser": "babel-eslint",
-  "plugins": [
-    "jest",
-    "import",
-    "@typescript-eslint"
+  'parser': 'babel-eslint',
+  'plugins': [
+    'jest',
+    'import',
+    '@typescript-eslint',
   ],
-  "env": {
-    "jest/globals": true,
-    "node": true,
-    "browser": true
+  'env': {
+    'jest/globals': true,
+    'node': true,
+    'browser': true,
   },
-  "overrides": [
+  'overrides': [
     {
-      "files": [
-        "**/*.tsx",
-        "**/*.ts"
+      'files': [
+        '**/*.tsx',
+        '**/*.ts',
       ],
-      "parser": "@typescript-eslint/parser"
-    }
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true,
-      "modules": true
+      'parser': '@typescript-eslint/parser',
+      'parserOptions': {
+        'project': './tsconfig.json',
+        'ecmaFeatures.jsx': true,
+      },
     },
-    "ecmaVersion": 2019,
-    "sourceType": "module"
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+      'modules': true,
+    },
+    'ecmaVersion': 2019,
+    'sourceType': 'module',
   },
-  "rules": {
-    "quotes": [
-      "error",
-      "single",
+  'rules': {
+    'quotes': [
+      'error',
+      'single',
       {
-        "avoidEscape": true
-      }
+        'avoidEscape': true,
+      },
     ],
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error",
-    "@typescript-eslint/no-object-literal-type-assertion": 0,
-    "import/no-unresolved": [
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    '@typescript-eslint/no-object-literal-type-assertion': 0,
+    'prettier/prettier': 0,
+    'no-console': 0,
+    'import/no-absolute-path': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    'no-extra-boolean-cast': 0,
+    'no-undef': [
+      0,
+      {},
+    ],
+    '@typescript-eslint/no-unused-vars': [
       2,
       {
-        "commonjs": true,
-        "ignore": [
-          ".js",
-          ".ts",
-          ".tsx"
-        ],
-        "caseSensitive": false
-      }
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_',
+      },
     ],
-    "react/jsx-filename-extension": [
-      "error",
+    'import/no-unresolved': [
+      2,
       {
-        "extensions": [
-          ".tsx",
-          ".js",
-          ".ts"
-        ]
-      }
-    ]
+        'commonjs': true,
+        'amd': true,
+        'ignore': [
+          '.js',
+          '.ts',
+          '.tsx',
+          '@',
+        ],
+        'caseSensitive': false,
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        'extensions': [
+          '.tsx',
+          '.js',
+          '.ts',
+        ],
+      },
+    ],
   },
-  "settings": {
-    "import/resolver": {
-      "babel-plugin-root-import": [
-        {
-          "rootPathPrefix": "@",
-          "rootPathSuffix": "src/"
-        }
-      ]
-    }
-  }
-}
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': [
+          '.ts',
+          '.tsx',
+          '.js',
+          '.json',
+        ],
+      },
+    },
+  },
+};
