9e37f93cec5bcfa20c04b1e2f8a0e02222481504
zusinShinpei <kmh011225@naver.com>
2019-12-05 15:04:38 +0900

    Cleanup Code, Bump Version

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 module.exports = {
   'extends': [
-    'eslint-config-react-native-wcandillon',
-    'plugin:json/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:json/recommended'
   ],
   'parser': 'babel-eslint',
   'plugins': [
-    'jest',
-    'import',
     '@typescript-eslint',
+    'eslint-plugin-react-hooks',
+    'import',
+    'jest',
     'json',
+    'react-native'
   ],
   'env': {
-    'jest/globals': true,
-    'node': true,
     'browser': true,
+    'jest/globals': true,
+    'node': true
   },
   'overrides': [
     {
       'files': [
         '**/*.tsx',
-        '**/*.ts',
+        '**/*.ts'
       ],
       'parser': '@typescript-eslint/parser',
       'parserOptions': {
         'project': './tsconfig.json',
-        'ecmaFeatures.jsx': true,
-      },
-    },
+        'ecmaFeatures.jsx': true
+      }
+    }
   ],
   'parserOptions': {
     'ecmaFeatures': {
       'jsx': true,
-      'modules': true,
+      'modules': true
     },
     'ecmaVersion': 2019,
-    'sourceType': 'module',
+    'sourceType': 'module'
   },
   'rules': {
     'quotes': [
       'error',
       'single',
       {
-        'avoidEscape': true,
-      },
+        'avoidEscape': true
+      }
     ],
     'jest/no-disabled-tests': 'warn',
     'jest/no-focused-tests': 'error',
@@ -57,18 +61,15 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 0,
     'max-len': 0,
     'no-extra-boolean-cast': 0,
-    'no-undef': [
-      0,
-      {},
-    ],
+    'no-undef': 0,
     '@typescript-eslint/no-unused-vars': [
       2,
       {
         'vars': 'all',
         'varsIgnorePattern': '^_',
         'args': 'after-used',
-        'argsIgnorePattern': '^_',
-      },
+        'argsIgnorePattern': '^_'
+      }
     ],
     '@typescript-eslint/no-non-null-assertion': 0,
     'import/no-unresolved': [
@@ -82,8 +83,8 @@ module.exports = {
           '.tsx',
           '@',
         ],
-        'caseSensitive': false,
-      },
+        'caseSensitive': false
+      }
     ],
     'react/jsx-filename-extension': [
       'error',
@@ -91,22 +92,62 @@ module.exports = {
         'extensions': [
           '.tsx',
           '.js',
-          '.ts',
-        ],
-      },
+          '.ts'
+        ]
+      }
     ],
     'json/*': [
       'error',
       {
         'allowComments': true,
-      },
+      }
     ],
+    'global-require': 0,
+    'no-use-before-define': ['error', { 'variables': false }],
+    'no-unused-vars': 0,
+    'sort-imports': ['error', {
+      'ignoreCase': false,
+      'ignoreDeclarationSort': true,
+      'ignoreMemberSort': false,
+      'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single']
+    }],
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-explicit-any': 2,
+    'react/jsx-props-no-spreading': 0,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 0,
+    'react-native/no-color-literals': 0,
+    'react-native/no-raw-text': 0,
+    'import/no-extraneous-dependencies': 2,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 2
   },
   'settings': {
     'import/resolver': {
       'node': {
-        'extensions': ['.ts', '.tsx', '.js', '.json'],
-      },
-    },
-  },
+        'extensions': [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.jsx',
+          '.android.ts',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.jsx',
+          '.ios.ts',
+          '.ios.tsx',
+          '.web.js',
+          '.web.jsx',
+          '.web.ts',
+          '.web.tsx'
+        ]
+      }
+    }
+  }
 };
