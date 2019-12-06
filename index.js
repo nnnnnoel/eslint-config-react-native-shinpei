56ac86932d794b21830215f0ef115c6bf9487089
zusinShinpei <kmh011225@naver.com>
2019-12-06 16:30:13 +0900

    Sort rules, Bump Version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
   'extends': [
     'airbnb',
-    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:json/recommended'
+    'plugin:json/recommended',
+    'plugin:prettier/recommended'
   ],
   'parser': 'babel-eslint',
   'plugins': [
@@ -27,8 +27,8 @@ module.exports = {
       ],
       'parser': '@typescript-eslint/parser',
       'parserOptions': {
-        'project': './tsconfig.json',
-        'ecmaFeatures.jsx': true
+        'ecmaFeatures.jsx': true,
+        'project': './tsconfig.json'
       }
     }
   ],
@@ -41,27 +41,15 @@ module.exports = {
     'sourceType': 'module'
   },
   'rules': {
-    'quotes': [
-      'error',
-      'single',
-      {
-        'avoidEscape': true
-      }
-    ],
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
-    '@typescript-eslint/no-object-literal-type-assertion': 0,
-    'prettier/prettier': 0,
-    'no-console': 0,
-    'import/no-absolute-path': 0,
-    '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/ban-ts-ignore': 0,
-    'max-len': 0,
-    'no-extra-boolean-cast': 0,
-    'no-undef': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-object-literal-type-assertion': 0,
     '@typescript-eslint/no-unused-vars': [
       2,
       {
@@ -71,7 +59,14 @@ module.exports = {
         'argsIgnorePattern': '^_'
       }
     ],
-    '@typescript-eslint/no-non-null-assertion': 0,
+    'global-require': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'import/no-absolute-path': 0,
+    'import/no-extraneous-dependencies': 2,
     'import/no-unresolved': [
       2,
       {
@@ -86,6 +81,33 @@ module.exports = {
         'caseSensitive': false
       }
     ],
+    'json/*': [
+      'error',
+      {
+        'allowComments': true
+      }
+    ],
+    'max-len': 0,
+    'no-console': 0,
+    'no-extra-boolean-cast': 0,
+    'no-nested-ternary': 0,
+    'no-undef': 0,
+    'no-unused-vars': 0,
+    'no-use-before-define': ['error', { 'variables': false }],
+    'prettier/prettier': 0,
+    'quotes': [
+      'error',
+      'single',
+      {
+        'avoidEscape': true
+      }
+    ],
+    'sort-imports': ['error', {
+      'ignoreCase': false,
+      'ignoreDeclarationSort': true,
+      'ignoreMemberSort': false,
+      'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single']
+    }],
     'react/jsx-filename-extension': [
       'error',
       {
@@ -96,35 +118,15 @@ module.exports = {
         ]
       }
     ],
-    'json/*': [
-      'error',
-      {
-        'allowComments': true,
-      }
-    ],
-    'global-require': 0,
-    'no-use-before-define': ['error', { 'variables': false }],
-    'no-unused-vars': 0,
-    'sort-imports': ['error', {
-      'ignoreCase': false,
-      'ignoreDeclarationSort': true,
-      'ignoreMemberSort': false,
-      'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single']
-    }],
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/indent': 0,
-    '@typescript-eslint/no-empty-interface': 0,
-    '@typescript-eslint/no-explicit-any': 2,
     'react/jsx-props-no-spreading': 0,
-    'react-native/no-unused-styles': 2,
-    'react-native/split-platform-components': 2,
-    'react-native/no-inline-styles': 0,
-    'react-native/no-color-literals': 0,
-    'react-native/no-raw-text': 0,
-    'import/no-extraneous-dependencies': 2,
+    "react/no-array-index-key": 0,
+    'react-hooks/exhaustive-deps': 2,
     'react-hooks/rules-of-hooks': 2,
-    'react-hooks/exhaustive-deps': 2
+    'react-native/no-color-literals': 0,
+    'react-native/no-inline-styles': 0,
+    'react-native/no-raw-text': 0,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2
   },
   'settings': {
     'import/resolver': {
